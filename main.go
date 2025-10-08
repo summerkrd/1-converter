eacb940c8abc70e0dd770ab7f2276ae4a6c6cf86
Anatoliy <karp.anatol@gmail.com>
2025-10-08 16:30:30 +0300

    начал 5.15 домашнее

--- main.go
+++ main.go
@@ -4,20 +4,31 @@ import "fmt"
 
 func main() {
 
+	const USD = 1
+	const EUR = 2
+	const RUB = 3
+
 	const USDToEUR = 0.8515
 	const USDToRUB = 81.9
 
 	EURToRUB := USDToRUB / USDToEUR
+	RubToEUR := USDToEUR / USDToRUB
 
-	fmt.Print(EURToRUB)
+	fmt.Println(EURToRUB)
+	fmt.Println(RubToEUR)
 
 }
 
-func userInput() (value float64) {
+func userInput() (count float64, original int, target int) {
 
-	fmt.Print("Введите количество валюты: ")
+	fmt.Println("Введите количество валюты: ")
+	fmt.Scan(&count)
 
-	fmt.Scan(&value)
+	fmt.Println("Из какой валюты перевести?\n(1-USD/2-EUR/3-RUB)")
+	fmt.Scan(&original)
+
+	fmt.Println("В какую валюту перевести?\n(1-USD/2-EUR/3-RUB)")
+	fmt.Scan(&target)
 
 	return
 }
