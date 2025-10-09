d64b558f08f6ea34c64db22060412db6a55794ca
Anatoliy <karp.anatol@gmail.com>
2025-10-09 13:11:10 +0300

    на проверку 5.15

--- main.go
+++ main.go
@@ -4,35 +4,92 @@ import "fmt"
 
 func main() {
 
-	const USD = 1
-	const EUR = 2
-	const RUB = 3
+	const (
+		USD = 1.0
+		EUR = 0.8
+		RUB = 81.9
+	)
 
-	const USDToEUR = 0.8515
-	const USDToRUB = 81.9
-
-	EURToRUB := USDToRUB / USDToEUR
-	RubToEUR := USDToEUR / USDToRUB
-
-	fmt.Println(EURToRUB)
-	fmt.Println(RubToEUR)
-
+	calculateCurrency(userInput(USD, EUR, RUB))
 }
 
-func userInput() (count float64, original int, target int) {
+func userInput(usd float64, eur float64, rub float64) (count float64, original float64, target float64) {
 
-	fmt.Println("Введите количество валюты: ")
-	fmt.Scan(&count)
+	const (
+		USDId = 1
+		EURId = 2
+		RUBId = 3
+	)
 
-	fmt.Println("Из какой валюты перевести?\n(1-USD/2-EUR/3-RUB)")
-	fmt.Scan(&original)
+	var originalID int
+	var targetID int
 
-	fmt.Println("В какую валюту перевести?\n(1-USD/2-EUR/3-RUB)")
-	fmt.Scan(&target)
+	for {
+		fmt.Printf("Из какой валюты перевести?\n(%d-USD/%d-EUR/%d-RUB)", USDId, EURId, RUBId)
+		_, err := fmt.Scan(&originalID)
 
-	return
+		if err != nil || originalID < USDId || originalID > RUBId {
+
+			fmt.Println("Incorrect data")
+			continue
+
+		} else {
+
+			break
+		}
+	}
+
+	for {
+		fmt.Println("Введите количество валюты: ")
+
+		_, err := fmt.Scan(&count)
+
+		if err != nil || count <= 0 {
+			fmt.Println("Incorrect data")
+			continue
+
+		} else {
+
+			break
+		}
+	}
+
+	for {
+		fmt.Printf("В какую валюту перевести?\n(%d-USD/%d-EUR/%d-RUB)", USDId, EURId, RUBId)
+		_, err := fmt.Scan(&targetID)
+
+		if err != nil || targetID < USDId || targetID > RUBId || targetID == originalID {
+			fmt.Println("Incorrect data")
+			continue
+
+		} else {
+
+			break
+		}
+	}
+
+	switch originalID {
+	case 1:
+		original = usd
+	case 2:
+		original = eur
+	case 3:
+		original = rub
+	}
+
+	switch targetID {
+	case 1:
+		target = usd
+	case 2:
+		target = eur
+	case 3:
+		target = rub
+	}
+
+	return count, original, target
 }
 
 func calculateCurrency(count float64, original float64, target float64) {
 
+	fmt.Print(count * (target / original))
 }
